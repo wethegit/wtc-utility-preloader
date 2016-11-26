a3dd12a1067791d870cab2aafd7b7b01b9aabd64
Scorpio Lam <scorpio@wethecollective.com>
2016-11-25 17:00:22 -0800

    update preloader to use promises or callbacks depending on if promises are supported.

--- src/es6/wtc-utility-preloader.js
+++ src/es6/wtc-utility-preloader.js
@@ -1,7 +1,7 @@
 
 /**
  * Simple Preloader
- * Simple preloader to load images.
+ * Simple preloader to load images using promises or callbacks.
  * Requirements: none
  * Usage:
  * var preloader = new window.wtc.utilities.Preloader();
@@ -10,48 +10,119 @@
  * preloader.add([{file: 'imagename.jpg', type:'image'}, {file: 'imagename.jpg', type:'image'}]);
  * preloader.load(callback);
  */
-class Preloader {
-  constructor(options) {
-    this.options = options;
-    this.files = [];
-    this.filesLoading = [];
-    this.loadCounter = 0;
-    this.progress = 0;
-    this.callback = function(){};
-  }
 
-  add(files) {
-    if(typeof files === 'string' || files instanceof String) {
-      let type = arguments[1] || 'image';
-      this.files.push({
-        file: files,
-        type:type
-      });
-    } else {
-      //appending files array to this.files array
-      this.files.push.apply(this.files, files);
-    }
-  }
-  onFilesLoaded(event,callback) {
-    this.loadCounter += 1;
-    if(this.loadCounter === this.filesLoading.length) {
-      this.callback();
-    }
-  }
+ class Preloader {
+   constructor({debug=false} = {}) {
+     let _files = [];
+     let _loadCounter = 0;
 
-  load(callback) {
-    let loaded = 0;
-    this.callback = callback;
+     this.debug = debug;
 
-    for(let num = 0; num < this.files.length; num++) {
-      if(!this.files[num].type || this.files[num].type == 'image') {
-        let img = new Image();
-        img.onload = this.onFilesLoaded.bind(this);
-        img.src = this.files[num].file;
-        this.filesLoading.push(img);
-      }
-    }
-  }
-}
+     this.filesLoading = [];
+     this.callback = function(){};
 
-export default Preloader;
+     this.addFiles = function(file) {
+       _files.push(file);
+     }
+     this.getFiles = function() {
+       return _files;
+     }
+
+     this.incrementLoadCounter = function() {
+       _loadCounter++;
+     }
+     this.getLoadCounter = function() {
+       return _loadCounter;
+     }
+
+
+   }
+
+   add(files) {
+     if(typeof files === 'string' || files instanceof String) {
+       let type = arguments[1] || 'image';
+       this.addFiles({
+         file: files,
+         type:type
+       });
+     } else {
+       //appending files array to _files array
+       _files.map(this.addFiles);
+     }
+   }
+
+   getImageWithPromise(file){
+     let that = this;
+     return new Promise(function(resolve,reject) {
+       let img = new Image();
+       img.onload = function(){
+         that.incrementLoadCounter();
+          if(that.debug) {
+            console.log("i'm loaded: " + file.file );
+          }
+         resolve(file);
+       };
+       img.onerror = function() {
+         if(that.debug) {
+           console.log("file not found: " + file.file);
+         }
+         resolve(file);
+       };
+       img.src = file.file;
+     });
+   }
+
+   loadWithPromises(callback) {
+     let that = this;
+     let f = that.getFiles();
+     //create an array of images
+     let imagePromises = f.map(this.getImageWithPromise.bind(this));
+     //wait until all images are loaded.
+     Promise.all(imagePromises).then(function(file){
+       callback();
+     }).catch(function(){
+       if(that.debug) {
+         console.log('there was an error');
+       }
+     });
+   }
+
+   loadWithCallbacks(callback) {
+     let f = this.getFiles();
+     for(let num = 0; num < f.length; num++) {
+       if(!f[num].type || f[num].type == 'image') {
+         let img = new Image();
+         img.addEventListener( 'load',this.onFilesLoaded.bind(this,callback),false);
+         img.addEventListener( 'error',this.onFilesLoaded.bind(this,callback),false);
+         img.src = f[num].file;
+         this.filesLoading.push(img);
+       }
+     }
+   }
+
+   onFilesLoaded(callback) {
+     if(this.debug) {
+       if(event.type === "load") {
+         console.log("i'm loaded: " + event.path[0].src);
+       } else {
+         console.log("file not found: " + event.path[0].src);
+       }
+
+     }
+     this.incrementLoadCounter();
+     if(this.getLoadCounter() === this.filesLoading.length && callback) {
+       callback();
+     }
+   }
+
+   load(callback) {
+     if (window.Promise) {
+       this.loadWithPromises(callback);
+     } else {
+       this.loadWithCallbacks(callback);
+     }
+   }
+ }
+
+
+ export default Preloader;
